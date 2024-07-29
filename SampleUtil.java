0193d9370ca2823ef83a3a51624ac08244a6c6e0
sharureddy6 <165276911+sharureddy6@users.noreply.github.com>
2024-07-29 11:50:08 +0530

    Add files via upload

---
+++ SampleUtil.java
@@ -0,0 +1,15 @@
+package com.testingmaven.sample;
+
+public class SampleUtil {
+	
+	public static Integer add(Integer a , Integer b){
+		if (a == null){
+			return null;
+		}
+		if (b == null){
+			return null;
+		}
+		return a + b;
+	}
+
+}
